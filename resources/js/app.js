3560915e1182f6a2621b8a983ee5c15c1e18ee87
DaniillGolovin <daniilgolovin656@gmail.com>
2024-09-01 20:56:31 +0300

    fix for github action

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../css/app.css';
 
 import Alpine from 'alpinejs';
 
