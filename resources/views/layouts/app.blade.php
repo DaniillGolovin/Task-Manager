cd3e17dac29e232544417c1b8fa621a41f4fc688
DaniillGolovin <daniilgolovin656@gmail.com>
2024-09-01 17:54:44 +0300

    added task status view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Менеджер задач') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,23 +15,15 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <body>
+        <div id="app">
+            <header class="fixed-top w-100">
+                <div class="container">
+                    @include('layouts.navigation')
+                </div>
+            </header>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+            @yield('content')
     </body>
 </html>
---
+++ resources/views/task_statuses/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+        <div class="grid col-span-full">
+            <h1 class="mb-5">
+                {{ __('status.Statuses') }}
+            </h1>
+            @include('flash::message')
+            @auth
+                <div>
+                    <a href="{{ route('task_statuses.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        {{ __('status.Create status') }}
+                    </a>
+                </div>
+            @endauth
+            <table class="mt-4">
+                <thead class="border-b-2 border-solid border-black text-left">
+                <tr>
+                    <th>ID</th>
+                    <th>{{ __('status.Status name') }}</th>
+                    <th>{{ __('status.Date of creation') }}</th>
+                    @auth
+                        <th>{{ __('status.Actions') }}</th>
+                    @endauth
+                </tr>
+                </thead>
+                @if ($statuses)
+                    @foreach ($statuses as $status)
+                        <tr class="border-b border-dashed text-left">
+                            <td>{{ $status->id }}</td>
+                            <td>{{ $status->name }}</td>
+                            <td>{{ $status->created_at->format('d.m.Y') }}</td>
+                            <td>
+                                @auth
+                                    @can('delete', $status)
+                                        <a data-confirm="Вы уверены?" data-method="delete"  class="text-red-600 hover:text-red-900 no-underline" href="{{ route('task_statuses.destroy', $status) }}">
+                                            {{ __('status.Delete') }}
+                                        </a>
+                                    @endcan
+                                    @can('update', $status)
+                                        <a class="text-blue-600 hover:text-blue-900 no-underline" href="{{ route('task_statuses.edit', $status->id) }}">{{ __('status.Edit') }}</a>
+                                    @endcan
+                                @endauth
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </table>
+            @if ($statuses->total() > $statuses->perPage())
+                {{ $statuses->links() }}
+            @endif
+        </div>
+    </div>
+</section>
+@endsection
