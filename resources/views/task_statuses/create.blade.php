be834292e9082ef9798965e162e2a34ce5dc2dbf
DaniillGolovin <daniilgolovin656@gmail.com>
2024-09-07 17:50:42 +0300

    fix css styles

--- resources/views/task_statuses/create.blade.php
+++ resources/views/task_statuses/create.blade.php
@@ -4,9 +4,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div class="grid col-span-full">
-            <h1 class="mb-5">
-                {{ __('status.Statuses') }}
-            </h1>
+            <h1 class="text-3xl font-bold mb-5 flex items-center">{{ __('status.Statuses') }}</h1>
             {!! Html::form('POST', route('task_statuses.store'))->class('w-50')->open() !!}
                 <div class="flex flex-col">
                     <div class="mt-2">
--- resources/views/task_statuses/index.blade.php
+++ resources/views/task_statuses/index.blade.php
@@ -4,9 +4,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div class="grid col-span-full">
-            <h1 class="mb-5">
-                {{ __('status.Statuses') }}
-            </h1>
+            <h1 class="text-3xl font-bold mb-5 flex items-center">{{ __('status.Statuses') }}</h1>
             @include('flash::message')
             @auth
                 <div>
@@ -34,13 +32,13 @@
                             <td>{{ $status->created_at->format('d.m.Y') }}</td>
                             <td>
                                 @auth
+                                    @can('update', $status)
+                                        <a class="text-blue-600 hover:text-blue-900 no-underline" href="{{ route('task_statuses.edit', $status->id) }}">{{ __('status.Edit') }}</a>
+                                    @endcan
                                     @can('delete', $status)
                                         <a data-confirm="Вы уверены?" data-method="delete"  class="text-red-600 hover:text-red-900 no-underline" href="{{ route('task_statuses.destroy', $status) }}">
                                             {{ __('status.Delete') }}
                                         </a>
-                                    @endcan
-                                    @can('update', $status)
-                                        <a class="text-blue-600 hover:text-blue-900 no-underline" href="{{ route('task_statuses.edit', $status->id) }}">{{ __('status.Edit') }}</a>
                                     @endcan
                                 @endauth
                             </td>
